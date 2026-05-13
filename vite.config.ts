26b287debd52b52837c7f3a7bc705e29c56e2f95
yesser <mhamdiyesser04@gmail.com>
2026-05-13 19:48:42 +0300

    feat: setup tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tanstackRouter(), tailwindcss()],
 });
