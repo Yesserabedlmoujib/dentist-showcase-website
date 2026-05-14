5c411335df226862ad86a95eabae90454d88b12b
yesser <mhamdiyesser04@gmail.com>
2026-05-14 03:31:12 +0300

    update main with routerProvide wrapping

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { routeTree } from "./routeTree.gen.ts";
 
-createRoot(document.getElementById('root')!).render(
+const router = createRouter({ routeTree });
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
